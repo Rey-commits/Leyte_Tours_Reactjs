7efa6c2976dc8056643fc07269cf65d6b46fc0d8
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-29 09:28:26 +0800

    tour info have been modified

--- src/tourData.js
+++ src/tourData.js
@@ -5,7 +5,7 @@ export const tourData = [
     img: "./img/newyork.jpeg",
     name: "new york bridge tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature. Its calm and clear waters makes it very inviting to go swimming and snorkeling. There are no private resorts in the island or any accommodations but camping is allowed. This is a paradise island and a dream weekend getaway in Leyte. Most people visit here just to enjoy the beautiful beach, stunning sandbar and amazing views."
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
     img: "./img/paris.jpeg",
     name: "paris lights tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Canigao Island is one of the most visited tourist spots in Leyte. It is a small uninhabited island surrounded with white sand beach and turquoise waters while its center is full of trees. One can walk around the island for just approximately 20 minutes. The best part is, it is very cheap to get here, one just have to pay around ‎₱40 for the entrance fee and ‎₱60 for the boat fare. One can either rent a cottage or pitch a tent if planning to stay overnight. This is also a perfect island destination in Leyte for swimming and snorkeling since it is abundant in coral reefs and marine life."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tourData = [
     img: "./img/london.jpeg",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Digyo Island is one of the most visited tourist spots in Leyte and a part of the group of four small islands called “Cuatro Islas” in Leyte. The other islands are called Mahaba, Apid and Himokilan Islands. Digyo Island is the smallest island in Cuatro Islas but it is dubbed as the most beautiful one. It has a long sandbar which is the main attraction of the island and crystal clear waters perfect for swimming. There are cottages available in the island but one can also pitch his own tent, seafood are also available. This is the first stop for all who wants to go on a day trip but most visitors tend to stay here for the night and explore the remaining islands the other day."
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const tourData = [
     img: "./img/tokyo.jpeg",
     name: "tokyo sushi tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Mahaba Island, as the name implies, it is elongated in shape and one of the beautiful islands in Cuatro Islas. It is an island covered with lush vegetation and beautiful white sand beaches and crystal clear waters. Most of the time, there is a noticeable sandbar parallel to the beach on the eastern side of the island. It also has beautiful rock formations and trees to have a shade when the sun gets too hot. Ask your boatmen to stop for a while on the spot where corals and marine life abounds if you want to go snorkeling."
   }
 ];
