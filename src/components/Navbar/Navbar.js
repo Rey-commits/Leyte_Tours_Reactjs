ca30fe497b895d61d9b7fb95153fb90de83a74e3
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-29 09:25:59 +0800

    navbar brand have been modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import logo from "../../logo.svg"
+// import logo from "../../logo.svg"
 import "./navbar.scss"
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <img src={logo} alt="city tours company"></img>
+            {/* <img src={logo} alt="city tours company"></img> */}
+            <div className="navBrand">Leyte Islands <span>Tours</span></div>
             <ul className="nav-links">
                 <li>
                     <a href="/" className="nav-link">home</a>
