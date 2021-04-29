6fc917d965d815ace53b01b355e79e24178fe836
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-29 09:40:22 +0800

    tour data have been modified

--- src/tourData.js
+++ src/tourData.js
@@ -1,34 +1,34 @@
 export const tourData = [
   {
     id: 1,
-    city: "new york",
-    img: "./img/newyork.jpeg",
-    name: "new york bridge tour",
+    city: "Polompon, Leyte",
+    img: "./img/kalanggaman.jpg",
+    name: "Kalanggaman Island Tour",
     info:
-      "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature. Its calm and clear waters makes it very inviting to go swimming and snorkeling. There are no private resorts in the island or any accommodations but camping is allowed. This is a paradise island and a dream weekend getaway in Leyte. Most people visit here just to enjoy the beautiful beach, stunning sandbar and amazing views."
+      "Kalanggaman Island one of the famous tourist spots in Leyte and it is being tagged as the most beautiful island in the province and in the entire Philippines. The long stretch of powdery white sandbars on both ends of the island is one of its amazing feature."
   },
   {
     id: 2,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    city: "Matalom, Leyte",
+    img: "./img/canigao.jpg",
+    name: "Canigao Island Tour",
     info:
-      "Canigao Island is one of the most visited tourist spots in Leyte. It is a small uninhabited island surrounded with white sand beach and turquoise waters while its center is full of trees. One can walk around the island for just approximately 20 minutes. The best part is, it is very cheap to get here, one just have to pay around ‎₱40 for the entrance fee and ‎₱60 for the boat fare. One can either rent a cottage or pitch a tent if planning to stay overnight. This is also a perfect island destination in Leyte for swimming and snorkeling since it is abundant in coral reefs and marine life."
+      "Canigao Island is one of the most visited tourist spots in Leyte. It is a small uninhabited island surrounded with white sand beach and turquoise waters while its center is full of trees. One can walk around the island for just approximately 20 minutes."
   },
   {
     id: 3,
-    city: "london",
-    img: "./img/london.jpeg",
-    name: "london royal palace tour",
+    city: "Cuatro Islas, Inopacan, Leyte",
+    img: "./img/digyo.jpg",
+    name: "Digyo Island Tour",
     info:
-      "Digyo Island is one of the most visited tourist spots in Leyte and a part of the group of four small islands called “Cuatro Islas” in Leyte. The other islands are called Mahaba, Apid and Himokilan Islands. Digyo Island is the smallest island in Cuatro Islas but it is dubbed as the most beautiful one. It has a long sandbar which is the main attraction of the island and crystal clear waters perfect for swimming. There are cottages available in the island but one can also pitch his own tent, seafood are also available. This is the first stop for all who wants to go on a day trip but most visitors tend to stay here for the night and explore the remaining islands the other day."
+      "Digyo Island is one of the most visited tourist spots in Leyte and a part of the group of four small islands called “Cuatro Islas” in Leyte. The other islands are called Mahaba, Apid and Himokilan Islands. Digyo Island is the smallest island in Cuatro Islas but it is dubbed as the most beautiful one."
   },
   {
     id: 4,
-    city: "tokyo",
-    img: "./img/tokyo.jpeg",
-    name: "tokyo sushi tour",
+    city: "Cuatro Islas, Inopacan, Leyte",
+    img: "./img/mahaba.jpg",
+    name: "Mahaba Island Tour",
     info:
-      "Mahaba Island, as the name implies, it is elongated in shape and one of the beautiful islands in Cuatro Islas. It is an island covered with lush vegetation and beautiful white sand beaches and crystal clear waters. Most of the time, there is a noticeable sandbar parallel to the beach on the eastern side of the island. It also has beautiful rock formations and trees to have a shade when the sun gets too hot. Ask your boatmen to stop for a while on the spot where corals and marine life abounds if you want to go snorkeling."
+      "Mahaba Island, as the name implies, it is elongated in shape and one of the beautiful islands in Cuatro Islas. It is an island covered with lush vegetation and beautiful white sand beaches and crystal clear waters."
   }
 ];
